3aaa51fbe7918ab6bbe36b250a7bdd7ac177314d
YuZzzze <1162367836@qq.com>
2024-01-21 15:09:16 +0800

    feat: locale and theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,20 @@
+import { RuntimeAntdConfig } from '@umijs/max';
+import { theme } from 'antd';
 import './i18n';
 
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: 'admin' };
 }
+
+export const antd: RuntimeAntdConfig = (memo) => {
+  if (!localStorage.getItem('theme')) {
+    localStorage.setItem('theme', 'light');
+  }
+  memo.theme ??= {};
+  memo.theme.algorithm =
+    localStorage.getItem('theme') === 'light'
+      ? theme.defaultAlgorithm
+      : theme.darkAlgorithm;
+
+  return memo;
+};
--- src/models/global.ts
+++ src/models/global.ts
@@ -1,13 +1,44 @@
-// 全局共享数据示例
-import { DEFAULT_NAME } from '@/constants';
-import { useState } from 'react';
+import { useAntdConfigSetter } from '@umijs/max';
+import { theme } from 'antd';
+import { changeLanguage } from 'i18next';
+import { useEffect, useState } from 'react';
+const { defaultAlgorithm, darkAlgorithm } = theme;
 
-const useUser = () => {
-  const [name, setName] = useState<string>(DEFAULT_NAME);
+const themeKeys = {
+  light: defaultAlgorithm,
+  dark: darkAlgorithm,
+};
+
+const useGlobal = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>(
+    (localStorage.getItem('theme') as 'light' | 'dark') || 'light',
+  );
+
+  const [language, setLanguage] = useState(
+    localStorage.getItem('language') ||
+      localStorage.getItem('i18nextLng')?.slice(0, 2),
+  );
+
+  const setAntdConfig = useAntdConfigSetter();
+
+  // 切换主题
+  useEffect(() => {
+    setAntdConfig({ theme: { algorithm: themeKeys[theme] } });
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  // 切换语言
+  useEffect(() => {
+    changeLanguage(language);
+    localStorage.setItem('language', language as string);
+  }, [language]);
+
   return {
-    name,
-    setName,
+    theme,
+    setTheme,
+    language,
+    setLanguage,
   };
 };
 
-export default useUser;
+export default useGlobal;
